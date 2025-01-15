2f896461375c2509916b3ea0a2ad2f5a98027148
duridev <duridev@outlook.com>
2025-01-14 23:44:06 -0300

    Extrayendo los valores de la cxonsulta previa

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,13 +1,25 @@
+import axios from 'axios'
+
 export default function useClima() {
 
-    const obtenerClima = ({ciudad, pais}) => {
+    const obtenerClima = async ({ciudad, pais}) => {
         // Importar el Api Key
         const key = import.meta.env.VITE_API_KEY
 
-        //Obtener Latitud y longitud
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        try {
+            //Obtener Latitud y longitud
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
-        // Obtener el Clima
+            const { data } = await axios(url)
+            const { lat, lon } = data[0]
+
+            // Obtener el Clima
+
+            
+        } catch (error) {
+            
+        }
+
     }
 
 
