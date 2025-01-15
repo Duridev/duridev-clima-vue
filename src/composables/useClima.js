29183467848e7c117ed32559df5aaae1a912120a
duridev <duridev@outlook.com>
2025-01-14 23:38:12 -0300

    Primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -5,6 +5,7 @@ export default function useClima() {
         const key = import.meta.env.VITE_API_KEY
 
         //Obtener Latitud y longitud
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
 
         // Obtener el Clima
     }
