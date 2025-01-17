4de5a79e84251ab4bb267e4e82eee8778e03ceb8
duridev <duridev@outlook.com>
2025-01-16 23:51:08 -0300

    correcion de codigo en clima de clima.name a clima.main

--- src/components/Clima.vue
+++ src/components/Clima.vue
@@ -14,10 +14,10 @@ defineProps({
 <template>
     <div class="resultado">
         <h2>Clima de: {{ clima.name }}</h2>
-        <p class="actual">{{ formatearTemperatura(clima.name.temp)}}&deg;C</p>
+        <p class="actual">{{ formatearTemperatura(clima.main.temp)}}&deg;C</p>
         <div class="temperatura">
-            <p>Min: <span>{{ formatearTemperatura(clima.name.temp_min)}}&deg;C</span></p>
-            <p>Max: <span>{{ formatearTemperatura(clima.name.temp_max)}}&deg;C</span></p>
+            <p>Min: <span>{{ formatearTemperatura(clima.main.temp_min)}}&deg;C</span></p>
+            <p>Max: <span>{{ formatearTemperatura(clima.main.temp_max)}}&deg;C</span></p>
         </div>
     </div>
 </template>
