7dc9041b37933bade02c8980388a07af994703cf
duridev <duridev@outlook.com>
2025-01-16 23:09:14 -0300

    mostrando el clima

--- src/components/Clima.vue
+++ src/components/Clima.vue
@@ -1,16 +1,23 @@
 <script setup>
+import useClima from '../composables/useClima'
 
+const { formatearTemperatura } = useClima()
+defineProps({
+    clima: {
+        type: Object,
+        required: true
+    }
+})
 </script>
 
 
 <template>
     <div class="resultado">
-        <h2>Clima de:</h2>
-        <p class="actual"></p>
+        <h2>Clima de: {{ clima.name }}</h2>
+        <p class="actual">{{ formatearTemperatura(clima.name.temp)}}&deg;C</p>
         <div class="temperatura">
-            <p>Min: <span>&deg;C</span></p>
-            <p>Max: <span>&deg;C</span></p>
+            <p>Min: <span>{{ formatearTemperatura(clima.name.temp_min)}}&deg;C</span></p>
+            <p>Max: <span>{{ formatearTemperatura(clima.name.temp_max)}}&deg;C</span></p>
         </div>
-
     </div>
 </template>
--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -32,9 +32,12 @@ export default function useClima() {
         return Object.values(clima.value).length > 0
     })
 
+    const formatearTemperatura = temperatura => parseInt(temperaatura - 273.15)
+
     return {
         obtenerClima,
         clima,
-        mostrarClima
+        mostrarClima,
+        formatearTemperatura
     }
 }
